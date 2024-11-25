5199020c337b2d10846c6d79183af23a5db08d52
Michał Malankiewicz <malankiewicz.m@gmail.com>
2024-11-25 20:10:36 +0100

    rozwiazane

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,13 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+        std::vector< char > result;
+        result.reserve(people.size());
+
+        for(auto it=people.rbegin(); it !=people.rend(); ++it)
+        {
+            it->birthday();
+            result.push_back(it->isMonster() ? 'n': 'y');
+        }
+    return result;
 }
